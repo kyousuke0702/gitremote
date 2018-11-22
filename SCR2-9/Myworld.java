8e03f5c2ef9a47a8136cd314b9a14b1b1b24a133
artifical-karage <suraimukubota@yahoo.co.jp>
2018-11-22 11:42:21 +0900

    up wdd

--- SCR2-9/Myworld.java
+++ SCR2-9/Myworld.java
@@ -21,16 +21,13 @@ public class Myworld extends World
         addObject(new enemy(),500,350);
         addObject(new enemy(),500,200);
         addObject(new enemy(),500,40);
-<<<<<<< HEAD
         addObject(new Pizza(),0,350);
         addObject(new Pizza(),0,200);
         addObject(new Pizza(),0,40);
-=======
+
         addObject(new jiki() ,300,200);
     }
     
     public void score(int sc){
-        
->>>>>>> 639284a70152495fd616aa67dbd7ec734fb3ee1d
     }
 }
--- SCR2-9/jiki.java
+++ SCR2-9/jiki.java
@@ -6,14 +6,25 @@ import greenfoot.*;
  * @author (your name) 
  * @version (a version number or a date)
  */
+
 public class jiki extends Actor
 {
+    static public int n,f;
+    
     /**
      * Act - do whatever the jiki wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        // Add your action code here.
+        
+        if (Greenfoot.isKeyDown( "shift" ))
+        {n = 2; }else {n = 5;}
+        
+        if(Greenfoot.isKeyDown( "up" ) ){
+            setRotation(270);
+            move(n);
+        
+        }
     }    
 }
