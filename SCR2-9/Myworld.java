a9c51ce6f698f591541b9b127edc621c9fae14a1
artifical-karage <suraimukubota@yahoo.co.jp>
2018-11-22 11:04:10 +0900

    penguin added

--- SCR2-9/Myworld.java
+++ SCR2-9/Myworld.java
@@ -17,5 +17,10 @@ public class Myworld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+                
+        addObject(new enemy(),500,350);
+        addObject(new enemy(),500,200);
+        addObject(new enemy(),500,40);
+      
     }
 }
